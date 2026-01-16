4290b99f7606336eb2af1f384e95c9380012911a
Nicklas Doran <nickdoran6@gmail.com>
2026-01-16 15:29:31 -0500

    Added Main file. Basic functionality, but needs api key

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-import pytesseract
-from PIL import Image
+import base64
+from openai import OpenAI
 
-print(pytesseract.image_to_string(Image.open("./Images/test1.png"))) 
+client = OpenAI()
 
+with open("./Images/test1.png", 'rb') as image_file:
+    image_bytes = image_file.read()
+
+image_base64 = base64.b64encode(image_bytes).decode('utf-8') 
+
+image_url = f"data:image/png;base64,{image_base64}"
+
+response = client.responses.create(
+    model="gpt-5-mini-2025-08-07",
+    input=[
+        {
+            'content': "What day is it in california USA",
+            'role': "user",
+        }
+    ]
+)
+
+
+print(response.output_text)
